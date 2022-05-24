6a3ebe567e580d80857954f8bec1488220e1a62e
Bryan <balegria.gonz@gmail.com>
2022-05-23 22:14:08 -0500

    Add user icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,8 @@ import {
     faEnvelope,
     faTrash,
     faPhone,
-    faEarthAmericas
+    faEarthAmericas,
+    faUser
 } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -16,14 +17,10 @@ function Contact(props) {
                             <img src='https://www.citypng.com/public/uploads/preview/address-contact-book-black-icon-11640520529mhwu7ngb7h.png' className='img-fluid rounded-start' alt='contact'/>
                         </div>
                             <div className='info-column'>
-                                <h5 className="card-title">{props.renderName}</h5>
+                                <h5 className="card-title"><FontAwesomeIcon icon={faUser} /> {props.renderName}</h5>
                                 <p className="card-text"><FontAwesomeIcon icon={faEnvelope} /> {props.renderEmail}</p>
                                 <p className="card-text"><FontAwesomeIcon icon={faPhone} /> {props.renderPhone}</p>
                                 <p className="card-text"><FontAwesomeIcon icon={faEarthAmericas} /> {props.renderCountry}</p>
-                                {/* <h5 className="card-name">Bryan Alegria</h5>
-                                <p className="card-email"><FontAwesomeIcon icon={faEnvelope} /> bryan@gmail.com</p>
-                                <p className="card-phone"><FontAwesomeIcon icon={faPhone} /> 946910544</p>
-                                <p className="card-country"><FontAwesomeIcon icon={faEarthAmericas} /> Peru</p> */}
                                 <button onClick={() => props.deleteHandler(props.id)}><FontAwesomeIcon icon={faTrash} /></button>
                             </div>
                 </div>
